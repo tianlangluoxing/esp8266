6d22d3400e95cea35d863af0078d08023d6c3e68
tianlangluoxing <tianlangluoxing@outlook.com>
2021-09-10 23:40:13 +0800

    新增web配置

--- Repeater/Repeater.ino
+++ Repeater/Repeater.ino
@@ -1,45 +1,47 @@
 /*
- * NAPT协议下的路由桥接
- * 
- * 这是一个最简单的版本
- * 打开STA模式,将esp8266连接到路由器
- * 再打开AP模式,生成一个AP热点
- * 再通过NAPT协议将AP热点的网络地址转接到STA网络上
- * 
- * 具体情况,请自行了解NAPT协议
- * 
- * 程序来源,esp8266 arduino例程的 RangeEatender-NAPT
- * 现只是进行了封装及添加了MAC地址修改部分(主要用于钉钉打卡)
- * 
- * 板砖者: 落星
- * 时间: 2021-8-26
- */
+   NAPT协议下的路由桥接
 
-#define HAVE_NETDUMP 1
+   这是一个最简单的版本
+   打开STA模式,将esp8266连接到路由器
+   再打开AP模式,生成一个AP热点
+   再通过NAPT协议将AP热点的网络地址转接到STA网络上
+
+   具体情况,请自行了解NAPT协议
+
+   程序来源,esp8266 arduino例程的 RangeEatender-NAPT
+   现只是进行了封装及添加了MAC地址修改部分(主要用于钉钉打卡)
+
+   板砖者: 落星
+   时间: 2021-8-26
+*/
 
 #ifndef STASSID
 #define STASSID "luoxing_2.4"
-#define STAPSK  "ABCluoxing123"
-#define APSSID "sxdmn1.com"
-#define APPSK  "z12345678"
+#define STAPASS  "ABCluoxing123"
+#define APSSID "sxdmn1.com1"
+#define APPASS  "z12345678"
 uint8_t newMACAddress[] = {0x10, 0xDA, 0x43, 0x70, 0x9A, 0xDD};
 #endif
 
 #include <ESP8266WiFi.h>
 #include <lwip/napt.h>
 #include <lwip/dns.h>
-#include <dhcpserver.h>
+#include <LwipDhcpServer.h>
+#include <ESP8266WebServer.h>
 #include "fileSystem.h"
+#include "HTML.h"
 
 #define NAPT 1000
 #define NAPT_PORT 10
 
+ESP8266WebServer server(80);
+
 /*
- * 配置你要连接的WiFi
- */
-void setSTA() {
+   配置你要连接的WiFi
+*/
+void setSTA(const char *ssid, const char *pass) {
   WiFi.mode(WIFI_STA);
-  WiFi.begin(STASSID, STAPSK);
+  WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
     delay(500);
@@ -51,28 +53,73 @@ void setSTA() {
 }
 
 /*
- * 配置你要开启的AP热点
- */
-void setAP() {
+   配置你要开启的AP热点
+*/
+void setAP(const char *ssid, const char *pass) {
   WiFi.softAPConfig(  // enable AP, with android-compatible google domain
     IPAddress(172, 217, 28, 254),
     IPAddress(172, 217, 28, 254),
     IPAddress(255, 255, 255, 0));
-  WiFi.softAP(APSSID, APPSK);
+  WiFi.softAP(ssid, pass);
   Serial.printf("AP: %s\n", WiFi.softAPIP().toString().c_str());
 }
+
 /*
- * 修改MAC地址
- */
-void setMAC(){
-  wifi_set_macaddr(SOFTAP_IF, &newMACAddress[0]);
+   小写转大写
+*/
+void lowerCase2uppercase(char *DEST, const char *SRC) {
+  int len = strlen(SRC);
+  for (int index = 0; index < len; index++) {
+    if (SRC[index] >= 'a' &&  SRC[index] <= 'z') {
+      DEST[index] = char( SRC[index] - ('a' - 'A'));
+    }
+    else {
+      DEST[index] = SRC[index];
+    }
+  }
+  DEST[len] = '\0';
+}
+
+/*
+   String类型的MAC地址转uint8_t类型的MAC地址
+*/
+void stringMAC2MAC(uint8_t *MAC, String S_MAC) {
+  char CS_MAC[20] = {0}; //= const_cast<char*>(S_MAC.c_str());
+  lowerCase2uppercase(CS_MAC, S_MAC.c_str()); //转为大写
+  //  Serial.println(CS_MAC);
+  char C_MAC[12] = {0};
+  char *result = NULL;
+  result = strtok(CS_MAC, ":");
+  while (result != NULL) {
+    strcat(C_MAC, result);
+    result = strtok(NULL, ":");
+  }
+  for (int index = 0; index < 6; index ++) {
+    uint8_t s1 = C_MAC[index * 2] - '0';
+    if (s1 > 9) {
+      s1 = s1 - 7;
+    }
+    uint8_t s2 = C_MAC[index * 2 + 1] - '0';
+    if (s2 > 9) {
+      s2 = s2 - 7;
+    }
+    MAC[index] = s1 * 16 + s2;
+  }
+}
+
+/*
+   修改MAC地址
+*/
+void setMAC(uint8_t *MAC) {
+  wifi_set_macaddr(SOFTAP_IF, &MAC[0]);
+  //  wifi_set_macaddr(SOFTAP_IF, &newMACAddress[0]);
   delay(200);
   uint8_t MACAddress[6];
-  if(wifi_get_macaddr(SOFTAP_IF,MACAddress)){
+  if (wifi_get_macaddr(SOFTAP_IF, MACAddress)) {
     Serial.print("MAC: ");
-    for(int i=0;i<6;i++){
-      Serial.print(MACAddress[i],HEX);
-      if(i<5){
+    for (int i = 0; i < 6; i++) {
+      Serial.print(MACAddress[i], HEX);
+      if (i < 5) {
         Serial.print(":");
       }
     }
@@ -80,16 +127,16 @@ void setMAC(){
   }
 }
 /*
- * 开启网络地址转换
- */
-void setNAPT(){
+   开启网络地址转换
+*/
+void setNAPT(const char *ssid, const char *pass) {
   err_t ret = ip_napt_init(NAPT, NAPT_PORT);
   Serial.printf("ip_napt_init(%d,%d): ret=%d (OK=%d)\n", NAPT, NAPT_PORT, (int)ret, (int)ERR_OK);
   if (ret == ERR_OK) {
     ret = ip_napt_enable_no(SOFTAP_IF, 1);
     Serial.printf("ip_napt_enable_no(SOFTAP_IF): ret=%d (OK=%d)\n", (int)ret, (int)ERR_OK);
     if (ret == ERR_OK) {
-      Serial.printf("WiFi Network '%s' with the password is  '%s'\n", APSSID, APPSK);
+      Serial.printf("WiFi Network '%s' with the password is  '%s'\n", ssid, pass);
     }
   }
   if (ret != ERR_OK) {
@@ -98,29 +145,118 @@ void setNAPT(){
 }
 
 void setup() {
-  
-  EEPROM.begin(512);
+
+  initE2prom();
   Serial.begin(115200);
+  Serial.println();
+
   Serial.printf("\n\nNAPT Range extender\n");
   Serial.printf("Heap on start: %d\n", ESP.getFreeHeap());
 
-  // first, connect to STA so we can get a proper local DNS server
-  setSTA();
+  /**
+     连接WiFi
+     如果有配置WiFi
+     则从配置中获取配置信息
+  */
+  char STA_SSIS[32] = {0};
+  char STA_PASS[32] = {0};
+  //  read_STA_SSID(STA_SSIS);
+  if (read_STA_SSID(STA_SSIS) > 1 && read_STA_PASS(STA_PASS)) {
+    setSTA(STA_SSIS, STA_PASS);
+  }
+  else {
+    setSTA(STASSID, STAPASS);
+  }
 
 
   // give DNS servers to AP side
-  dhcps_set_dns(0, WiFi.dnsIP(0));
-  dhcps_set_dns(1, WiFi.dnsIP(1));
+  dhcpSoftAP.dhcps_set_dns(0, WiFi.dnsIP(0));
+  dhcpSoftAP.dhcps_set_dns(1, WiFi.dnsIP(1));
 
-  setAP();
-  setMAC();
-  
+  /**
+       开启WiFi热点
+       如果有配置WiFi热点
+       则从配置中获取配置信息
+  */
+  char AP_SSID[32] = {0};
+  char AP_PASS[32] = {0};
+  bool AP_flag = false;
+  if (read_AP_SSID(AP_SSID) > 1 && read_AP_PASS(AP_PASS)) {
+    setAP(AP_SSID, AP_PASS);
+    AP_flag = true;
+  }
+  else {
+    setAP(APSSID, APPASS);
+    AP_flag = false;
+  }
+
+
+  //  uint8_t _MAC[6] = {0};
+  //  String S_MAC = "de:a0:4a:79:82:1c";
+  //  stringMAC2MAC(_MAC, S_MAC);
+  //  save_MAC((char*)_MAC);
+
+  uint8_t MAC[6] = {0};
+  if (read_MAC((char*)MAC) > 0) {
+    setMAC((uint8_t*)MAC);
+  }
+
+
   Serial.printf("Heap before: %d\n", ESP.getFreeHeap());
-  setNAPT();
-  
+  if (AP_flag) {
+    setNAPT(AP_SSID, AP_PASS);
+  }
+  else {
+    setNAPT(APSSID, APPASS);
+  }
+
+
   Serial.printf("Heap after napt init: %d\n", ESP.getFreeHeap());
- 
+
+  server.on("/", HTTP_GET, []() {
+    server.send(200, "text/html", index_html);
+  });
+
+  server.on("/config", HTTP_POST, []() {
+    if (server.hasArg("ssid") && server.hasArg("pass") && server.hasArg("ap_ssid") && server.hasArg("ap_pass")) {
+      Serial.println(String(server.arg("ssid")));
+      Serial.println(String(server.arg("pass")));
+      Serial.println(String(server.arg("ap_ssid")));
+      Serial.println(String(server.arg("ap_pass")));
+      Serial.println(String(server.arg("MAC")));
+      save_STA_SSID((String(server.arg("ssid"))).c_str());
+      save_STA_PASS((String(server.arg("pass"))).c_str());
+      save_AP_SSID((String(server.arg("ap_ssid"))).c_str());
+      save_AP_PASS((String(server.arg("ap_pass"))).c_str());
+      uint8_t _MAC[6] = {0};
+      String S_MAC = server.arg("MAC");
+      stringMAC2MAC(_MAC, S_MAC);
+      save_MAC((char*)_MAC);
+      //      save_MAC((String(server.arg("MAC"))).c_str());
+
+      //      char STA_SSIS[32] = {0};
+      //      char STA_PASS[32] = {0};
+      //      char AP_SSID[32] = {0};
+      //      char AP_PASS[32] = {0};
+      //      uint8_t MAC[6] = {0};
+      //      read_STA_SSID(STA_SSIS);
+      //      read_STA_PASS(STA_PASS);
+      //      read_AP_PASS(AP_PASS);
+      //      read_AP_PASS(AP_PASS);
+      //      read_MAC((char*)MAC);
+
+      server.send(200, "application/json", "{\"data\":{\"status\":200}}");
+      delay(1000);
+      ESP.reset();
+    }
+    server.send(200, "application/json", "{\"data\":{\"status\":400}}");
+  });
+  server.onNotFound([]() {
+    server.send(404, "text/plain", "NotFound");
+  });
+  server.begin();
 }
 
 void loop() {
+  server.handleClient();
 }
--- Repeater/fileSystem.h
+++ Repeater/fileSystem.h
@@ -11,34 +11,97 @@
 #define MAC_ADDR        160
 #define EEPROM_LEN      256
 
-void initE2prom(){
+void initE2prom() {
   EEPROM.begin(EEPROM_LEN);
 }
 
-bool isConfig(){
-  if(EEPROM.read(CONFIG_FLAG) == 1){
+bool isConfig() {
+  if (EEPROM.read(CONFIG_FLAG) == 1) {
     return true;
-  }else{
+  } else {
     return false;
   }
 }
 
+void config() {
+  EEPROM.write(CONFIG_FLAG, 1);
+}
+
 void writeData(uint8_t addr, const char* data, uint8_t len) {
   EEPROM.write(addr, len);
+  Serial.println("start write");
   for (int index = 1; index <= len; index ++) {
-    EEPROM.write(addr + index, *data ++);
+    EEPROM.write(addr + index, data[index - 1]);
+    Serial.print(addr + index);
   }
+  EEPROM.commit();
+  Serial.println("\nend write");
 }
 
-void readData(uint8_t addr, const char* data){
+void readData(uint8_t addr, char* data) {
   uint8_t len = EEPROM.read(addr);
-  for (int index = 1; index <= len; index ++) {
-    *data++ = EEPROM.read(addr + index);
+
+  //  Serial.printf("read start %d, length:%d\n", addr, EEPROM.read(addr));
+  //  Serial.printf("read start %d, length:%d\n", addr, len);
+  if (len > 30 || len == 0) {
+    data = {0};
+    return;
   }
+  for (int index = 1; index <= len; index ++) {
+    data[index - 1] = EEPROM.read(addr + index);
+  }
+  data[len] = '\0';
+  //  Serial.printf("read start %d, Data:%s\n", addr, data);
 }
 
-void resetSystem(){
-  for(int index = 0; index < EEPROM_LEN; index++){
+
+void save_STA_SSID(const char* ssid) {
+  writeData(SSID_STA_ADDR, ssid, strlen(ssid));
+}
+uint8_t read_STA_SSID(char *ssid) {
+  readData(SSID_STA_ADDR, ssid);
+  Serial.printf("read_STA_SSID: %s\n", ssid);
+  return strlen(ssid);
+}
+
+void save_STA_PASS(const char* pass) {
+  writeData(PASS_STA_ADDR, pass, strlen(pass));
+}
+uint8_t read_STA_PASS(char *pass) {
+  readData(PASS_STA_ADDR, pass);
+  Serial.printf("read_STA_PASS: %s\n", pass);
+  return strlen(pass);
+}
+
+void save_AP_SSID(const char* ssid) {
+  writeData(SSID_AP_ADDR, ssid, strlen(ssid));
+}
+uint8_t read_AP_SSID(char *ssid) {
+  readData(SSID_AP_ADDR, ssid);
+  Serial.printf("read_AP_SSID: %s\n", ssid);
+  return strlen(ssid);
+}
+
+void save_AP_PASS(const char* pass) {
+  writeData(PASS_AP_ADDR, pass, strlen(pass));
+}
+uint8_t read_AP_PASS(char *pass) {
+  readData(PASS_AP_ADDR, pass);
+  Serial.printf("read_AP_PASS: %s\n", pass);
+  return strlen(pass);
+}
+
+void save_MAC(const char* MAC) {
+  writeData(MAC_ADDR, MAC, strlen(MAC));
+}
+uint8_t read_MAC(char *MAC) {
+  readData(MAC_ADDR, MAC);
+  Serial.printf("read_MAC: %x\n", MAC);
+  return strlen(MAC);
+}
+
+void resetSystem() {
+  for (int index = 0; index < EEPROM_LEN; index++) {
     EEPROM.write(index, 0);
   }
 }
